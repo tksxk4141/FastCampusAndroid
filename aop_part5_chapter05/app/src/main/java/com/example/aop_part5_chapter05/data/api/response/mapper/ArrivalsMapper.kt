a9302b606fbf864366d2a9ba5c9b6e25312a2c20
tksxk4141 <tksxk4141@naver.com>
2023-01-27 16:01:40 +0900

    지하철 도착 정보

--- aop_part5_chapter05/app/src/main/java/com/example/aop_part5_chapter05/data/api/response/mapper/ArrivalsMapper.kt
+++ aop_part5_chapter05/app/src/main/java/com/example/aop_part5_chapter05/data/api/response/mapper/ArrivalsMapper.kt
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-private val apiDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.'0'", Locale.KOREA)
+private val apiDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA)
 private val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.KOREA)
 
 private const val INVALID_FIELD = "-"
